d43b914d684bcb199502b1de334cb366a6a101cc
hmc-cricket-s18 <cbutler@cs.hmc.edu>
2019-04-20 14:30:39 -0700

    Image uploading
    probably not working but we'll see

--- FrontEnd/app/__init__.py
+++ FrontEnd/app/__init__.py
@@ -1,5 +1,10 @@
-from flask import Flask
+import os
 from config import Config
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+
+
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object(Config)
